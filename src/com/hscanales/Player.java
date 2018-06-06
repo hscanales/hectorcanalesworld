83549ba828490de0ac268cbe1b0b7200abc48a89
Hector Canales <00136317@uca.edu.sv>
2018-06-05 19:13:02 -0600

    Add Player Class and Player Interface

--- src/com/hscanales/Player.java
+++ src/com/hscanales/Player.java
@@ -5,11 +5,33 @@
  */
 package com.hscanales;
 
+import com.hscanales.clans.Clan;
+
 /**
  *
  * @author HsCanales <00136317@uca.edu.sv>
  */
-public class Player {
-    
+public class Player implements IPlayer{
+    Clan clan;
+
+    @Override
+    public void seleccionarClan() {
+    }
+
+    @Override
+    public void terminarTurno() {
+    }
+
+    @Override
+    public void Construir() {
+    }
+
+    @Override
+    public void defender() {
+    }
+
+    @Override
+    public void atacar() {
+    }
     
 }
